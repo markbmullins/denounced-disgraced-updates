b25bd250c92299d405aecbac2d9d838c1aa3a6b3
Mark Mullins <markmullins95@gmail.com>
2023-09-09 20:34:26 -0400

    made spotify player responsive

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,15 @@
-import { FunctionComponent, useEffect, useRef, useState } from "react";
+import {FunctionComponent, useEffect, useRef, useState} from "react";
 import styled from "styled-components";
 import Hamburger from "hamburger-react";
-import { TransparentLogo as TransparentLogo } from "../Logos";
-import { desktopAndLandscape, tablet } from "../../utils/mediaQueries";
-import { ColumnCentered } from "../../components/Layouts";
-import { Link } from "../../components/Link";
+import {TransparentLogo as TransparentLogo} from "../Logos";
+import {desktopAndLandscape, tablet} from "../../utils/mediaQueries";
+import {ColumnCentered} from "../../components/Layouts";
+import {Link} from "../../components/Link";
 import useNavbarColorOnScroll from "../../utils/hooks/useNavbarColorOnScroll";
 
 interface NavbarProps {
-  height: number;
-  pages: Record<string, string>;
+    height: number;
+    pages: Record<string, string>;
 }
 
 const Nav = styled.nav`
@@ -21,7 +21,7 @@ const Nav = styled.nav`
 
   height: ${(props) => props.height + "px"};
   background-color: ${(props) =>
-    props.isTransparent ? "transparent" : "#1a1a1a"};
+          props.isTransparent ? "transparent" : "#1a1a1a"};
 
   padding: 1vh;
   font-family: Bruno;
@@ -44,12 +44,11 @@ const Links = styled.ul`
 
 const NavLogoContainer = styled.div`
   width: 150px;
-  height: 52px;
-
+  height: 53px;
   position: relative;
   margin-right: auto;
   margin-left: 5%;
-  margin-top: 10px;
+  max-height: 70px;
 
   ${tablet} {
     width: 200px;
@@ -76,7 +75,7 @@ const SideDrawer = styled.div<{ isOpen: boolean }>`
   right: ${(props) => (props.isOpen ? "0" : "-100%")};
   width: 40%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.9);
+  background-color: #1a1a1a;
   z-index: 99;
   transition: right 0.3s ease-in-out;
   padding: 1rem;
@@ -87,50 +86,50 @@ const SideDrawer = styled.div<{ isOpen: boolean }>`
   }
 `;
 
-export const Navbar: FunctionComponent<NavbarProps> = ({ height, pages }) => {
-  const { isTransparent } = useNavbarColorOnScroll();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+export const Navbar: FunctionComponent<NavbarProps> = ({height, pages}) => {
+    const {isTransparent} = useNavbarColorOnScroll();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const links = Object.entries(pages).map(([pageName, url]) => (
-    <div key={url} onClick={() => setIsOpen(false)}>
-      <Link url={url} title={pageName} />
-    </div>
-  ));
+    const links = Object.entries(pages).map(([pageName, url]) => (
+        <div key={url} onClick={() => setIsOpen(false)}>
+            <Link url={url} title={pageName}/>
+        </div>
+    ));
 
-  const drawerRef = useRef(null);
+    const drawerRef = useRef(null);
 
-  useEffect(() => {
-    const handleOutsideClick = (e) => {
-      if (
-        isOpen &&
-        drawerRef.current &&
-        !drawerRef.current.contains(e.target)
-      ) {
-        setIsOpen(false);
-      }
-    };
-    document.addEventListener("click", handleOutsideClick);
-    return () => {
-      document.removeEventListener("click", handleOutsideClick);
-    };
-  }, [isOpen]);
-  return (
-    <Nav height={height} isTransparent={isTransparent}>
-      <NavLogoContainer>
-        <TransparentLogo />
-      </NavLogoContainer>
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if (
+                isOpen &&
+                drawerRef.current &&
+                !drawerRef.current.contains(e.target)
+            ) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener("click", handleOutsideClick);
+        return () => {
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, [isOpen]);
+    return (
+        <Nav height={height} isTransparent={isTransparent}>
+            <NavLogoContainer>
+                <TransparentLogo/>
+            </NavLogoContainer>
 
-      <Links>{links}</Links>
-      <HamburgerContainer onClick={(e) => e.stopPropagation()}>
-        <Hamburger
-          toggle={() => setIsOpen(!isOpen)}
-          toggled={isOpen}
-          size={20}
-        />
-      </HamburgerContainer>
-      <SideDrawer isOpen={isOpen} ref={drawerRef}>
-        <ColumnCentered>{links}</ColumnCentered>
-      </SideDrawer>
-    </Nav>
-  );
+            <Links>{links}</Links>
+            <HamburgerContainer onClick={(e) => e.stopPropagation()}>
+                <Hamburger
+                    toggle={() => setIsOpen(!isOpen)}
+                    toggled={isOpen}
+                    size={20}
+                />
+            </HamburgerContainer>
+            <SideDrawer isOpen={isOpen} ref={drawerRef}>
+                <ColumnCentered>{links}</ColumnCentered>
+            </SideDrawer>
+        </Nav>
+    );
 };
--- components/SpotifyPlayer/SpotifyPlayer.tsx
+++ components/SpotifyPlayer/SpotifyPlayer.tsx
@@ -1,36 +1,41 @@
 import React from "react";
-import styled from "styled-components";
+import {useWindowSize} from "../../utils/hooks/useWindowSize";
 
 export const SpotifyPlayer = () => {
-  return (
-    <>
-      <iframe
-        style={{ borderRadius: "12px" }}
-        src="https://open.spotify.com/embed/playlist/4WfemMhwoMW7nKEAnJ2TFN?utm_source=generator&theme=0"
-        width="450px"
-        height="352"
-        frameBorder="0"
-        allowFullScreen={false}
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
-    </>
-  );
+    const {width} = useWindowSize()
+    const max = 450
+    return (
+        <>
+            <iframe
+                style={{borderRadius: "12px"}}
+                src="https://open.spotify.com/embed/playlist/4WfemMhwoMW7nKEAnJ2TFN?utm_source=generator&theme=0"
+                width={width < max ? width - 30 : max}
+                height={width < max ? 370 : max}
+                frameBorder="0"
+                allowFullScreen={false}
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+            ></iframe>
+        </>
+    );
 };
 
 export const SpotifyPlayerLarge = () => {
-  return (
-    <>
-      <iframe
-        style={{ borderRadius: "12px" }}
-        src="https://open.spotify.com/embed/playlist/4WfemMhwoMW7nKEAnJ2TFN?utm_source=generator&theme=0"
-        width="600"
-        height="600"
-        frameBorder="0"
-        allowFullScreen={false}
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
-    </>
-  );
+    const {width} = useWindowSize()
+    const max = 600
+
+    return (
+        <>
+            <iframe
+                style={{borderRadius: "12px"}}
+                src="https://open.spotify.com/embed/playlist/4WfemMhwoMW7nKEAnJ2TFN?utm_source=generator&theme=0"
+                width={width < max ? width - 30 : max}
+                height={width < max ? 400 : max}
+                frameBorder="0"
+                allowFullScreen={false}
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+            ></iframe>
+        </>
+    );
 };
