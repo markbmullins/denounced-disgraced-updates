a1f3c77dfb56a5a47557afb24bc4e4baec22119b
Mark Mullins <markmullins95@gmail.com>
2023-09-22 18:58:22 -0400

    update product shape

--- src/server/routers/products/schema.ts
+++ src/server/routers/products/schema.ts
@@ -2,16 +2,27 @@ import { z } from "zod";
 
 export const ProductSchema = z.object({
   id: z.string(),
-  type: z.union([
-    z.literal("T-shirt"),
-    z.literal("Hoodie"),
-    z.literal("Tank Top"),
-    z.literal("Long Sleeve"),
+  productLine: z.literal("just-a-corpse"),
+  productType: z.union([
+    z.literal("t-shirt"),
+    z.literal("hoodie"),
+    z.literal("tank-top"),
+    z.literal("long-sleeve"),
+    z.literal("phone-case"),
+    z.literal("shorts"),
   ]),
-  design: z.union([
-    z.literal("JAC"),
-    z.literal("Design B"),
-    z.literal("Design C"),
+  artStyle: z.union([
+    z.literal("full-color"),
+    z.literal("black-and-white-red-outline"),
+    z.literal("black-and-white-green-outline"),
+    z.literal("mixed-red-outline"),
+    z.literal("mixed-green-outline"),
+  ]),
+  productColor: z.union([
+    z.literal("black"),
+    z.literal("white"),
+    z.literal("red"),
+    z.literal("green"),
   ]),
   price: z.number(),
   imageUrl: z.string().optional(),
