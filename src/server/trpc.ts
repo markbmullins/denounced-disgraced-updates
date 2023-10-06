061446b30aadf0783dce6149fafa22bdd52b92a9
skobaidze <sbkobaidze@gmail.com>
2023-10-07 01:57:02 +0400

    test

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -2,7 +2,6 @@ import { initTRPC } from "@trpc/server";
 
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
-// For instance, the use of a t variable
 // is common in i18n libraries.
 const t = initTRPC.create();
 
